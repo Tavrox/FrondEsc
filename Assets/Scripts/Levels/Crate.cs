3a0735f7d5188df254455b9788a97146becbcd30
Helari <babas-91@hotmail.fr>
2013-11-08 11:33:12 +0100

    ok

--- Assets/Scripts/Levels/Crate.cs
+++ Assets/Scripts/Levels/Crate.cs
@@ -3,13 +3,50 @@ using System.Collections;
 
 public class Crate : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private bool grounded =true;
+	[HideInInspector] public Transform thisTransform;
+	[SerializeField] private float gravityY;
+	[SerializeField] private Vector3 vectorMove;
+	//RaycastHit hitInfo;
+	//Ray landingRay;	
+	//public float deployHeight;
 	
+	public virtual void Awake()
+	{
+		thisTransform = transform;	
+	}
+	
+	public virtual void Start () 
+	{
+		StartCoroutine(StartGravity());
+	}
+	
+	IEnumerator StartGravity()
+	{
+		// wait for things to settle before applying gravity
+		yield return new WaitForSeconds(0.1f);
+		gravityY = 52f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{		
+		if(grounded == false)
+		{
+			vectorMove.y -= gravityY * Time.deltaTime;
+		}
+		//landingRay = new Ray(thisTransform.position, Vector3.down);
+	}
 	
+	void OnTriggerEnter(Collider other) 
+	{
+		if(other.gameObject.CompareTag("Ground")) 
+		{
+			grounded = true;
+		}
+		if(other.gameObject.CompareTag("Platforms")) 
+		{
+			grounded = true;
+		}
 	}
 }
